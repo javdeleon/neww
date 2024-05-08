575222b207ebd8710409e368370c9fec8e3f587a
Javie De Leon <116146894+javdeleon@users.noreply.github.com>
2024-05-08 21:47:22 +0800

    Add files via upload

---
+++ psychonnect/src/App.js
@@ -0,0 +1,13 @@
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+import Register from './Register';
+
+function App() {
+
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
